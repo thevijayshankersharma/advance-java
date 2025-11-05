70cee9fe54e50f5bd21ef1a2a9a89a6c194b9288
thevijayshankersharma <vijayshankersharma1973@gmail.com>
2025-11-05 19:45:29 +0530

    HttpServlet Program

--- demohttpservlet/src/main/java/demohttpservlet/FormServlet.java
+++ demohttpservlet/src/main/java/demohttpservlet/FormServlet.java
@@ -1,6 +1,10 @@
 package demohttpservlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,10 +15,34 @@ public class FormServlet extends HttpServlet {
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int eid = Integer.parseInt(req.getParameter("eid"));
 		String name = req.getParameter("name");
-		int age = Integer.parseInt(req.getParameter("age"));
+		double sal = Double.parseDouble(req.getParameter("sal"));
+
+		Connection con = null;
+		PreparedStatement ps = null;
 		
-		System.out.println("Name: "+ name);
-		System.out.println("Age: "+ age);
+		try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","root","root");
+            ps = con.prepareStatement("insert into emp values (?,?,?)");
+            
+            ps.setInt(1,eid);
+            ps.setString(2, name);
+            ps.setDouble(3, sal);
+
+            ps.executeUpdate();
+            
+            System.out.println("Record has been added to database");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (ps != null) ps.close();
+                if (con != null) con.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 	}
 }
