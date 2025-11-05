15a44041e87bd1e7ec34817681c023d40f7ed357
thevijayshankersharma <vijayshankersharma1973@gmail.com>
2025-11-05 19:34:12 +0530

    HttpServlet Program

---
+++ demohttpservlet/src/main/java/demohttpservlet/FormServlet.java
@@ -0,0 +1,20 @@
+package demohttpservlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FormServlet extends HttpServlet {
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name = req.getParameter("name");
+		int age = Integer.parseInt(req.getParameter("age"));
+		
+		System.out.println("Name: "+ name);
+		System.out.println("Age: "+ age);
+	}
+}
